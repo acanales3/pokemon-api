41fb51f5a5daf7df8d3da3e7d626b34599db38d7
acanales3 <alexander.s.canales@gmail.com>
2024-01-15 14:55:50 -0600

    change in UI of card

--- pokemon-api/components/PokemonCard.tsx
+++ pokemon-api/components/PokemonCard.tsx
@@ -8,11 +8,13 @@ export function PokemonCard({ name }: PokemonCardProps) {
   return (
     <Link
       href={name}
-      className="group m-3 text-center rounded-lg border border-transparent px-5 py-4 transition-colors border-gray-500 hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+      className="group m-3 text-center rounded-lg border border-transparent px-5 py-4 transition-colors border-gray-400 hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
       key={name + "Card"}
       rel="noopener noreferrer"
     >
-      <h2 className="text-2xl font-semibold">{name}</h2>
+      <h2 className="text-2xl font-semibold">
+        {name.charAt(0).toUpperCase() + name.slice(1)}
+      </h2>
     </Link>
   );
 }
