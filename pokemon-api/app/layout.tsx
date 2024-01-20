36b677c3eb7206b91d698ac753463dd1d81d9c42
acanales3 <alexander.s.canales@gmail.com>
2024-01-19 20:55:02 -0600

    remove themes

--- pokemon-api/app/layout.tsx
+++ pokemon-api/app/layout.tsx
@@ -19,11 +19,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main className="flex min-h-screen flex-col items-center p-24">
+        <main className="flex min-h-screen flex-col items-center p-24 bg-red-300">
           <div className="z-10 max-w-5xl w-full items-center justify-between text-sm">
             <Link href="/">
-              <h2 className="text-2xl text-bold text-center my-4">
-                Pokemon Finder
+              <h2 className="text-3xl font-bold text-center text-red-500 my-4">
+                Pok&eacute;mon Finder
               </h2>
             </Link>
           </div>
