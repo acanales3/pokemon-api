147c5b657271e8870a9cb275a7438be20e17aa68
acanales3 <alexander.s.canales@gmail.com>
2024-01-21 20:09:35 -0600

    size issues

--- pokemon-api/app/page.tsx
+++ pokemon-api/app/page.tsx
@@ -37,120 +37,120 @@ export default async function Home() {
       <Accordion className="w-full" type="single" collapsible>
         <AccordionItem
           value="item-1"
-          className="mx-auto w-full bg-red-200 rounded-lg overflow-hidden my-4 border-b-0"
+          className="w-full bg-red-200 rounded-lg overflow-hidden my-4 border-b-0"
         >
           <AccordionTrigger className="ml-2 exo2-ib">
             Generation One
           </AccordionTrigger>
-          <AccordionContent className="mx-auto max-w-screen-md">
+          <AccordionContent className="mx-auto max-w-screen-md text-center">
             <PokemonGrid generation={"One"} pokemonList={gen1} />
           </AccordionContent>
         </AccordionItem>
 
         <AccordionItem
           value="item-2"
-          className="mx-auto w-full bg-orange-200 rounded-lg overflow-hidden my-4 border-b-0"
+          className="w-full bg-orange-200 rounded-lg overflow-hidden my-4 border-b-0"
         >
           <AccordionTrigger className="ml-2 exo2-ib">
             Generation Two
           </AccordionTrigger>
-          <AccordionContent className="mx-auto max-w-screen-md">
+          <AccordionContent className="mx-auto max-w-screen-md text-center">
             <PokemonGrid generation={"Two"} pokemonList={gen2} />
           </AccordionContent>
         </AccordionItem>
 
         <AccordionItem
           value="item-3"
-          className="mx-auto w-full bg-orange-300 rounded-lg overflow-hidden my-4 border-b-0"
+          className="w-full bg-orange-300 rounded-lg overflow-hidden my-4 border-b-0"
         >
           <AccordionTrigger className="ml-2 exo2-ib">
             Generation Three
           </AccordionTrigger>
-          <AccordionContent className="mx-auto max-w-screen-md">
+          <AccordionContent className="mx-auto max-w-screen-md text-center">
             <PokemonGrid generation={"Three"} pokemonList={gen3} />
           </AccordionContent>
         </AccordionItem>
 
         <AccordionItem
           value="item-4"
-          className="mx-auto w-full bg-yellow-200 rounded-lg overflow-hidden my-4 border-b-0"
+          className="w-full bg-yellow-200 rounded-lg overflow-hidden my-4 border-b-0"
         >
           <AccordionTrigger className="ml-2 exo2-ib">
             Generation Four
           </AccordionTrigger>
-          <AccordionContent className="mx-auto max-w-screen-md">
+          <AccordionContent className="mx-auto max-w-screen-md text-center">
             <PokemonGrid generation={"Four"} pokemonList={gen4} />
           </AccordionContent>
         </AccordionItem>
 
         <AccordionItem
           value="item-5"
-          className="mx-auto w-full bg-green-200 rounded-lg overflow-hidden my-4 border-b-0"
+          className="w-full bg-green-200 rounded-lg overflow-hidden my-4 border-b-0"
         >
           <AccordionTrigger className="ml-2 exo2-ib">
             Generation Five
           </AccordionTrigger>
-          <AccordionContent className="mx-auto max-w-screen-md">
+          <AccordionContent className="mx-auto max-w-screen-md text-center">
             <PokemonGrid generation={"Five"} pokemonList={gen5} />
           </AccordionContent>
         </AccordionItem>
 
         <AccordionItem
           value="item-6"
-          className="mx-auto w-full bg-blue-200 rounded-lg overflow-hidden my-4 border-b-0"
+          className="w-full bg-blue-200 rounded-lg overflow-hidden my-4 border-b-0"
         >
           <AccordionTrigger className="ml-2 exo2-ib">
             Generation Six
           </AccordionTrigger>
-          <AccordionContent className="mx-auto max-w-screen-md">
+          <AccordionContent className="mx-auto max-w-screen-md text-center">
             <PokemonGrid generation={"Six"} pokemonList={gen6} />
           </AccordionContent>
         </AccordionItem>
 
         <AccordionItem
           value="item-7"
-          className="mx-auto w-full bg-blue-300 rounded-lg overflow-hidden my-4 border-b-0"
+          className="w-full bg-blue-300 rounded-lg overflow-hidden my-4 border-b-0"
         >
           <AccordionTrigger className="ml-2 exo2-ib">
             Generation Seven
           </AccordionTrigger>
-          <AccordionContent className="mx-auto max-w-screen-md">
+          <AccordionContent className="mx-auto max-w-screen-md text-center">
             <PokemonGrid generation={"Seven"} pokemonList={gen7} />
           </AccordionContent>
         </AccordionItem>
 
         <AccordionItem
           value="item-8"
-          className="mx-auto w-full bg-purple-200 rounded-lg overflow-hidden my-4 border-b-0"
+          className="w-full bg-purple-200 rounded-lg overflow-hidden my-4 border-b-0"
         >
           <AccordionTrigger className="ml-2 exo2-ib">
             Generation Eight
           </AccordionTrigger>
-          <AccordionContent className="mx-auto max-w-screen-md">
+          <AccordionContent className="mx-auto max-w-screen-md text-center">
             <PokemonGrid generation={"Eight"} pokemonList={gen8} />
           </AccordionContent>
         </AccordionItem>
 
         <AccordionItem
           value="item-9"
-          className="mx-auto w-full bg-purple-300 rounded-lg overflow-hidden my-4 border-b-0"
+          className="w-full bg-purple-300 rounded-lg overflow-hidden my-4 border-b-0"
         >
           <AccordionTrigger className="ml-2 exo2-ib">
             Generation Nine
           </AccordionTrigger>
-          <AccordionContent className="mx-auto max-w-screen-md">
+          <AccordionContent className="mx-auto max-w-screen-md text-center">
             <PokemonGrid generation={"Nine"} pokemonList={gen9} />
           </AccordionContent>
         </AccordionItem>
 
         <AccordionItem
           value="item-10"
-          className="mx-auto w-full bg-purple-500 rounded-lg overflow-hidden my-4 border-b-0"
+          className="w-full bg-purple-500 rounded-lg overflow-hidden my-4 border-b-0"
         >
           <AccordionTrigger className="ml-2 exo2-ib">
             Other Pokemon Forms!
           </AccordionTrigger>
-          <AccordionContent className="mx-auto max-w-screen-md">
+          <AccordionContent className="mx-auto max-w-screen-md text-center">
             <PokemonGrid
               special={true}
               generation={"Other"}
